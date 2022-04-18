e89bd6f6618012949f15f0bbb9e276b0253d615a
suraj-mane <sun.mane1100@gmail.com>
2022-04-18 20:19:35 +0530

    complete

---
+++ block-BNaadt/registration-app/routes/users.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var Users = require('../models/user');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log(req.session);
+  res.render('users');
+});
+
+/* GET users register */
+router.get('/register', (req,res,next) => {
+  var error = req.flash('error')[0];
+  res.render('register', {error});
+})
+
+/* POST Create users*/
+router.post('/register', (req,res,next) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  Users.findOne({email}, (err,data) => {
+    if(data) {
+      req.flash('error','Email is already exist');
+      res.redirect('/users/register')
+    } else {
+      Users.create(req.body, (err,user) => {
+        if(err) return next(err);
+        res.redirect('/users/login');
+      })
+    }
+  })
+})
+
+/* GET user login */
+router.get('/login', (req,res,next) => {
+  var error = req.flash('error')[0];
+  res.render('login', {error});
+})
+
+/* POST Login */
+router.post('/login', (req,res,next) => {
+  var {email, password} = req.body;
+  if(!email || !password){
+    req.flash('error', 'Email and Password is require')
+    return res.redirect('/users/login');
+  }
+  Users.findOne({email}, (err, user) => {
+    if(err) return next(err);
+    if(!user) {
+      req.flash('error', 'Email is not register')
+      return res.redirect('/users/login');
+    }
+    user.verifyPassword(password, (err, result) => {
+      if(err) return next(err);
+      if(!result) {
+        req.flash('error', 'Password is not Match')
+        return res.redirect('/users/login');
+      }
+      req.session.userId = user.id;
+      res.redirect('/users');
+    })
+  })
+})
+
+module.exports = router;
